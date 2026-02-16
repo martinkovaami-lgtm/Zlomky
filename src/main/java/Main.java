a32a69a26699a324214e3d2fa30355b9a9f29739
martian5 <martian5@uhk.cz>
2026-02-16 12:34:42 +0100

    2. cviceni % soucet, rozdil

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,14 +5,23 @@ public class Main {
         IO.println("Projekt zlomky"); //novinka Javy 25
         IO.println("============================");
 
+
         var a = new Zlomek();
 
-        a.setCitatel(3);
-        a.setJmenovatel(4);
 
-        IO.println("Zlomek :"+a.getCitatel()+" / "+a.getJmenovatel());
+        a.setCitatel(1);
+        a.setJmenovatel(2);
 
-        Zlomek b = new Zlomek(4, 5);
+        IO.println("Zlomek :" + a.getCitatel() + " / " + a.getJmenovatel());
+
+        Zlomek b = new Zlomek(3, 4);
         IO.println(b);
+
+        var soucet = a.plus(b);
+        IO.println("Soucet je %s ".formatted(soucet.zkratit()));
+        var rozdil = a.minus(b);
+        IO.println("Rozdil je %s ".formatted(rozdil));
+
+
     }
 }
--- src/main/java/cz/uhk/zlomky/Zlomek.java
+++ src/main/java/cz/uhk/zlomky/Zlomek.java
@@ -4,19 +4,24 @@ public class Zlomek {
     private int citatel;
     private int jmenovatel;
 
-    public Zlomek(){}
+    public Zlomek() {
+    }
 
     public Zlomek(int citatel, int jmenovatel) {
         this.citatel = citatel;
         this.jmenovatel = jmenovatel;
     }
 
+    // defaultni konstruktor
+
+
     // selektory= getry = metody pro čteni dat
     public int getCitatel() {
 
         return citatel;
     }
-    public int getJmenovatel(){
+
+    public int getJmenovatel() {
         return jmenovatel;
     }
 
@@ -25,7 +30,8 @@ public class Zlomek {
     public void setCitatel(int citatel) {
         this.citatel = citatel;
     }
-    public void setJmenovatel(int jmenovatel){
+
+    public void setJmenovatel(int jmenovatel) {
 
         this.jmenovatel = jmenovatel;
         ;
@@ -38,4 +44,53 @@ public class Zlomek {
                 citatel, jmenovatel
         );
     }
+
+    /*
+    Metoda pro scitani dvou zlomku
+    param druhy
+    return novy zlomek, ktery je souctem dvou zlomku
+
+     */
+    public Zlomek plus(Zlomek druhy) {
+        int jm = jmenovatel * druhy.jmenovatel;
+        int cit = citatel * druhy.jmenovatel + druhy.citatel * jmenovatel;
+        return new Zlomek(cit, jm);
+    }
+
+    public Zlomek zkratit() {
+        int delitel = nsd(citatel, jmenovatel);
+        return new Zlomek(citatel / delitel, jmenovatel / delitel);
+    }
+
+    public Zlomek minus(Zlomek druhy) {
+        int jm = jmenovatel * druhy.jmenovatel;
+        int cit = citatel*druhy.jmenovatel - druhy.citatel*jmenovatel;
+        return new Zlomek(cit, jm);
+    }
+
+    /**
+     * vypocet nejvetsiho spolecnyho delitele cisel a a b
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    private int nsd(int a, int b) {
+        if (a < b) {
+            var pom = a;
+            a = b;
+            b = pom;
+        }
+        int zbytek;
+        do {
+            zbytek = a % b;
+            a = b;
+            b = zbytek;
+        }
+            while (zbytek != 0) ;
+            return a;
+
+
+    }
+
 }
